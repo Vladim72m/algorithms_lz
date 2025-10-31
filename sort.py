2141d67666ed0cc96603440b0ff2360a703979e3
Vladim72m <vladim72dx@tutamail.com>
2025-10-31 19:55:58 +0300

    sort.py

--- sort.py
+++ sort.py
@@ -1,35 +1,35 @@
 import random
-list = []
-q = 0 
-for i in range(0, 9):
-    list.append(random.randint(1, 1000000))
-print(list) 
-for i in range(9-1):
-    for j in range(9-1-i):
+list = [] # список 
+q = 0 # счетчик сравнений 
+for i in range(100):
+    list.append(random.randint(1, 1000000))  # в список добавляется случайное число от 1 до 1000000
+print(list)  # вывод неотсортированного списка
+for i in range(99): #выполняем сортировку пузырьком
+    for j in range(99-i):
+        q += 1
         if list[j] < list[j+1]:
-            list[j], list[j+1] = list[j+1], list[j]
-            q += 1
+            list[j], list[j+1] = list[j+1], list[j]  # меняем местами
             print(list)
-print ('////////////////////////')
 
 
-list1 = []
-d = 0 
-for i in range(0, 9):
-    list1.append(random.randint(1, 1000000))
+list1 = [] # список
+d = 0 # счетчик сравнений 
+for i in range(100):
+    list1.append(random.randint(1, 1000000))  
 print(list1) 
 a = 0 
-while a < 9 - 1:
+while a < 99:
     m = a 
     w = a + 1
-    while w < 9:
+    while w < 100:
         if list1[w] < list1[m]:
             m = w
         w += 1
+        d += 1
     list1[a], list1[m] = list1[m], list1[i]
     a += 1
-    d += 1
     print(list1)
 print ('Количество сравнений, сделанных в сортировке пузырьком:', q)
 print('Количество сравнений, сделанных в сортировке выбором', d)
     
+
